dfd5beb4141612499439fd8d02c4d913c2e84970
AguedaSierra <aguee224@gmail.com>
2018-03-23 18:23:01 +0100

    Actualizando programa1

--- openfda-1/Programa1.py
+++ openfda-1/Programa1.py
@@ -6,5 +6,6 @@ headers = {'User-Agent': 'http-client'}
 conn = http.client.HTTPSConnection("api.fda.gov")
 conn.request('GET', '/drug/label.json?search=results.openfda:spl_id&limit=1', None, headers)
 r1 = conn.getresponse()
+print(r1)
 for elem in r1:
     print(elem)
