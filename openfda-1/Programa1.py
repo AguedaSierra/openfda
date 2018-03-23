c1f53da1941024be9cd6aeab8d5d8e7c5bc65a51
Águeda Sierra <aguee224@gmail.com>
2018-03-23 11:03:48 +0100

    Actualizando programa1

--- openfda-1/Programa1.py
+++ openfda-1/Programa1.py
@@ -4,6 +4,6 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("https://api.fda.gov/drug/event.json?")
-conn.request('GET', 'search=patient.reaction.reactionmeddrapt:"fatigue"+AND+occurcountry:"ca"&limit=1', body=None)
+conn.request('GET', 'search="results":"openfda":"spl_id"AND"generic_name"', body=None)
 r1 = conn.getresponse()
 print(r1)
