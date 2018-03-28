9374ce93304ef45d68fccd83b0bb866db0f7bf72
Águeda Sierra <aguee224@gmail.com>
2018-03-28 22:37:36 +0200

    Actualizando programa1

--- openfda-2/Programa3.py
+++ openfda-2/Programa3.py
@@ -4,7 +4,7 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request('GET', '/drug/label.json?search=results.openfda.generic_name="acetylsalicylic%20acid"&limit=100', None, headers)
+conn.request('GET', '/drug/label.json?search=results.openfda.generic_name="salicylic%20acid"&limit=100', None, headers)
 r1 = conn.getresponse()
 r2 = r1.read().decode("utf-8")
 conn.close()
@@ -14,4 +14,8 @@ manufacturer = []
 for elem in data["results"]:
     if "manufacturer_name" in elem["openfda"]:
         manufacturer.append(str(elem["openfda"]["manufacturer_name"]))
-print(set(manufacturer))
+
+a = 0
+for name in set(manufacturer):
+    a = a+1
+    print("Fabricante nº", a, name, "\n", end=" ")
