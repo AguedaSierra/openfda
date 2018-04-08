f016e1d4a090c0bac049b8124cc1d51187193e7d
Águeda Sierra <aguee224@gmail.com>
2018-04-08 21:02:55 +0200

    Actualizando openfda-3

--- openfda-3/Cliente.py
+++ openfda-3/Cliente.py
@@ -1,11 +1,14 @@
+# A través de este cliente se puede ver cómo se recibe la información con los tag del código html.
+# Sin embargo también nos podemos conectar al servidor mediante cualquier navegador y así ver cómo se interpreta el html.
+
 import http.client
 
 PORT = 8000
 headers=('Content-type', 'text/html')
 
-conn = http.client.HTTPConnection('localhost', PORT) #El programa se conecta con el servidor
-conn.request("GET", "/") #Envia un mensaje de solicitud con un GET
-r1 = conn.getresponse() #Lee el mensaje de respuesta que ha recibido del servidor
-print(r1.status, r1.reason) #Se imprime el estado de la respuesta
-data1 = r1.read().decode("utf-8") #Se lee el contenido de la respuesta y se convierte a una cadena
-print(data1) #Se imprime el fichero html que ha recibido
+conn = http.client.HTTPConnection('localhost', PORT) # El cliente se conecta con el servidor
+conn.request("GET", "/") # Envía un mensaje de solicitud con un GET
+r1 = conn.getresponse() # Lee el mensaje de respuesta que ha recibido del servidor
+print(r1.status, r1.reason) # Se imprime el estado de la respuesta
+data1 = r1.read().decode("utf-8") # Se lee el contenido de la respuesta y se convierte a una cadena
+print(data1) # Se imprime el código html que ha recibido
--- openfda-3/Servidor.py
+++ openfda-3/Servidor.py
@@ -4,66 +4,68 @@ import http.client
 import json
 
 PORT = 8000
-class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler): #Se define la clase del manejador
+class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler): # Se define la clase del manejador
 
-    def do_GET(self): #Se define el comportamiento del servidor al recibir el GET de un cliente
+    def do_GET(self): # Se define el comportamiento del servidor al recibir el GET de un cliente
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
         headers = {'User-Agent': 'http-client'}
-        conn = http.client.HTTPSConnection("api.fda.gov") #El programa se conecta con la página de la api fda
-        conn.request('GET', '/drug/label.json?&limit=10', None, headers) #Con un GET pide la información de la etiqueta
-        #de diez medicamentos (limit=10) en un fichero json
+        conn = http.client.HTTPSConnection("api.fda.gov") # Se define el nombre del servidor al que queremos conectarnos
+        conn.request('GET', '/drug/label.json?&limit=10', None, headers) # Con un GET pide la información de la etiqueta
+        # de diez medicamentos (limit=10) en un fichero json
         r1 = conn.getresponse()
-        r2 = r1.read().decode("utf-8") #Lee la respuesta y la decodifica en formato utf-8
-        conn.close() #Se cierra la conexión
-        data = json.loads(r2) #La respuesta en código utf-8 se convierte en un diccionario para que sea más
-        #fácil de trabajar en Python
+        r2 = r1.read().decode("utf-8") # Lee la respuesta y se convierte a una cadena
+        conn.close()
+        data = json.loads(r2) # La respuesta se convierte en un diccionario para que sea más fácil de trabajar en Python
 
-        contenido = """
+        cont_inicial = """
               <!doctype html>
               <html>
-              <body style='background-color: lavender'>
-                <h1>Nombres de medicamentos:</h2>
+              <body style='background-color: darkblue'>
+              <font color = "white">
+                <h1>Nombres de medicamentos:</h1>
+                <h2>Se muestra el id del medicamento y su nombre</h2>
+            """
+
+        contenido = cont_inicial
+        for elem in data["results"]: # Se itera sobre los elementos que tienen como clave "results"
+            contenido += "Id: " + elem["id"] + " ---- Nombre: "
+            if "brand_name" in elem["openfda"]: # Si el medicamento tiene el nombre del fabricante
+                contenido += (str(elem["openfda"]["brand_name"]).lower()[2:-2]) # Se añade el nombre a la variable
+                # sin comillas ni corchetes ([2:-2]) y en minúsculas
+                contenido += "</br>"
+            else:
+                contenido += "no hay registros" + "</br>"
+        contenido += """
+              </body>
+              </html>
+        """
+
+        if self.path == "/" or self.path == "/medicamentos": # Si el parámetro del GET es "/" o "/medicamentos"
+            message = contenido # y el mensaje que se manda es su contenido
+        else:
+            message = cont_inicial + "Path incorrecto (debe ser / o /medicamentos)" # Si no el mensaje es error
+            message += """
               </body>
               </html>
             """
 
-        for elem in data["results"]: #Se itera sobre los elementos que tienen como clave "results"
-        #Dentro de esos valores hay más diccionarios
-            contenido += "El nombre del fabricante del medicamento con id: "
-            contenido += elem["id"]
-            contenido += " es: "
-            if "brand_name" in elem["openfda"]: #Si el medicamento tiene el nombre del fabricante
-                contenido += (str(elem["openfda"]["brand_name"]).lower()[2:-2]) #Se añade el nombre a la variable
-                # sin comillas ni corchetes ([2:-2]) y además separados por comas
-                contenido += "</br></body></html>"
-            else:
-                contenido += "no hay registros"
-                contenido += "</br></body></html>"
-
-        if self.path == "/" or self.path == "/medicamentos": #Si el parámetro del GET es "/" o "/medicamentos"
-            message = contenido #y el mensaje que se manda es su contenido
-        else:
-            message = "Error" #Si no el mensaje es error
-
-        #Se envía el mensaje completo
+        # Se envía el mensaje completo
         self.wfile.write(bytes(message, "utf8"))
         print("File served!")
         return
 
-#El servidor comienza a aquí
-#Se establece como manejador la clase definida anteriormente
+# Se establece como manejador la clase definida anteriormente
 Handler = testHTTPRequestHandler
 
-#Se configura el socket del servidor, para esperar conexiones de clientes
+# Se configura el socket del servidor, para esperar conexiones de clientes
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
 
-    #Entrar en el bucle principal
-    #Las peticiones se atienden desde el manejador
-    #Cada vez que se ocurra un "GET" se invoca al metodo do_GET del manejador
+    # Las peticiones se atienden desde el manejador
+    # Cada vez que se reciba un "GET" se llama al metodo do_GET del manejador
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
