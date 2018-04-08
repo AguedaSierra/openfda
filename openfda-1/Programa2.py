7729e73a669d686bb2575f19453f5b6c8f706c2a
Águeda Sierra <aguee224@gmail.com>
2018-04-08 19:43:54 +0200

    Actualizando programa2

--- openfda-1/Programa2.py
+++ openfda-1/Programa2.py
@@ -3,17 +3,15 @@ import json
 
 headers = {'User-Agent': 'http-client'}
 
-conn = http.client.HTTPSConnection("api.fda.gov") #El programa se conecta con la página de la api fda
-conn.request('GET', '/drug/label.json?&limit=10', None, headers) #Con un GET pide la información de la etiqueta
-#de diez medicamentos (limit=10) en un fichero json
+conn = http.client.HTTPSConnection("api.fda.gov") # Se define el nombre del servidor al que queremos conectarnos
+conn.request('GET', '/drug/label.json?&limit=10', None, headers) # Con un GET pide la información de la etiqueta
+# de diez medicamentos (limit=10) en un fichero json
 r1 = conn.getresponse()
-r2 = r1.read().decode("utf-8") #Lee la respuesta y la decodifica en formato utf-8
+r2 = r1.read().decode("utf-8") # Lee la respuesta y se convierte a una cadena
 conn.close()
-data = json.loads(r2) #La respuesta en código utf-8 se convierte en un diccionario para que sea más
-#fácil de trabajar en Python
+data = json.loads(r2) # La respuesta se convierte en un diccionario para que sea más fácil de trabajar en Python
 
-a = 0 #Se inicializa una variable para ir numerando los medicamentos
-for elem in data["results"]: #Se itera sobre los elementos que tienen como clave "results"
-    #Dentro de esos valores hay más diccionarios
+a = 0 # Se inicializa una variable para ir numerando los medicamentos
+for elem in data["results"]: # Se itera sobre los elementos que tienen como clave "results"
     a = a + 1
-    print("El identificador del objeto nº", a, "es:", elem["id"]) #Se obtiene el valor cuya clave es "id"
+    print("El identificador del objeto nº", a, "es:", elem["id"]) # Se imprime el valor cuya clave es "id"
