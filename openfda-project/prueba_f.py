7ef3fb3f4dd2a66375fdf722dd625dfcad53b1d5
AguedaSierra <aguee224@gmail.com>
2018-04-20 18:27:21 +0200

    Actualizando openfda_project

---
+++ openfda-project/prueba_f.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route("/searchDrug")
+def get_ingredient():
+    name = request.args.get('active_ingredient')
+    ingrediente = """ "substance_name": [
+          "SILICON DIOXIDE"
+        ]"""
+    return ingrediente + name
+
+@app.route("/searchCompany")
+def get_company():
+    name = request.args.get('company')
+    empresa = """ "manufacturer_name": [
+          "Rxhomeo Private Limited d.b.a. Rxhomeo, Inc"
+        ]"""
+    return empresa + name
+
+@app.route("/listDrugs")
+def get_drugs():
+    #name = request.args.get('drug')
+    medicamentos = """ "substance_name": [
+          "SILICON DIOXIDE"
+        ]"""
+    return medicamentos
+
+@app.route("/listCompanies")
+def get_listcomp():
+    #name = request.args.get('drug')
+    empresas = "Rxhomeo Private Limited d.b.a. Rxhomeo, Inc"
+    return empresas
+
+
+if __name__ == "__main__":
+    app.run()
