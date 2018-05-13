21a2ab8da0d255509b778c4fed5a942fe26b5498
Águeda Sierra <aguee224@gmail.com>
2018-05-13 23:49:41 +0200

    Actualizando openfda-project

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -1,124 +1,163 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask import request
+from flask_api import status
 import json
 import http.client
 
 app = Flask(__name__)
 
+conn = http.client.HTTPSConnection("api.fda.gov")
+
+class OpenFDAClient(): #Incluye la lógica para comunicarse con la API de OpenFDA
+    def __init__(self, enlace=""):
+        self.enlace = enlace
+    def fda_connection(self, enlace):
+        headers = {'User-Agent': 'http-client'}
+        conn.request('GET', enlace, None, headers)
+        r1 = conn.getresponse()
+        return r1
+
+class OpenFDAParser(): #Lógica para obtener los datos de los medicamentos
+    def __init__(self, r1=""):
+        self.r1 = r1
+    def get_data(self):
+        r2 = self.r1.read().decode("utf-8")
+        conn.close()
+        data = json.loads(r2)
+        return data
+
+class OpenFDAHTML(): #Generación del HTML para la visualización de la información
+    def __init__(self, datos=""" """):
+        #Inicializamos los datos
+        self.datos = datos
+
+
+    def convert_into_html(self, clave, subclave):
+        if clave == "warnings":
+            encabezado = "Warnings de los medicamentos:"
+        else:
+            if subclave == "brand_name":
+                encabezado = "Nombres de los medicamentos:"
+            else:
+                encabezado = "Nombres de las empresas:"
+
+        info = """
+                <!DOCTYPE html>
+                    <html>
+                        <body>
+                        <h1>""" + encabezado + """</h1>
+                        <ul>
+                        """
+
+        if "results" in self.datos:
+            for elem in self.datos["results"]:
+                if subclave != "":
+                    if subclave in elem[clave]:
+                        info += "<li>"
+                        info += (str(elem[clave][subclave]).lower()[2:-2])
+                        info += "</li>"
+                    else:
+                        info += "<li>"
+                        info += "No hay resultados"
+                        info += "</li>"
+                else:
+                    if clave in elem:
+                        info += "<li>"
+                        info += (str(elem[clave]).lower()[2:-2])
+                        info += "</li>"
+                    else:
+                        info += "<li>"
+                        info += "No hay resultados"
+                        info += "</li>"
+
+        else:
+            info += "<li>Error</li>"
+
+        info += """
+                        </ul>
+                        </body>
+                    </html>
+                    """
+        return info
+
 @app.route("/searchDrug")
 def get_ingredient():
-    nombre = request.args.get('active_ingredient')
-    act_ing = nombre
-    act_ing = act_ing.replace(" ", "%20")
-    datos = ""
-    temp = '/drug/label.json?search=active_ingredient:"' + act_ing + '"&limit=100'
-    headers = {'User-Agent': 'http-client'}
-    conn = http.client.HTTPSConnection("api.fda.gov")
-    conn.request('GET', temp, None, headers)
-    r1 = conn.getresponse()
-    r2 = r1.read().decode("utf-8")
-    conn.close()
-    data = json.loads(r2)
+   nombre = request.args.get('active_ingredient')
+   act_ing = nombre
+   act_ing = act_ing.replace(" ", "%20")
 
-    if "results" in data:
-        for elem in data["results"]:
-            if "brand_name" in elem["openfda"]:
-                datos += (str(elem["openfda"]["brand_name"]).lower()[2:-2])
-                datos += "<br>"
-            else:
-                datos += "No hay resultados"
-                datos += "<br>"
+   direccion = OpenFDAClient()
+   respuesta = direccion.fda_connection('/drug/label.json?search=active_ingredient:"' + act_ing + '"&limit=10')
 
-    else:
-        datos = "nada"
-    return "<ul>{}</ul>".format(datos)
+   data1 = OpenFDAParser(respuesta)
+   data = data1.get_data()
+
+   datos_html = OpenFDAHTML(data)
+   datos = datos_html.convert_into_html("openfda", "brand_name")
+
+   return datos
 
 @app.route("/searchCompany")
 def get_company():
     empresa = request.args.get('company')
     emp = empresa
     emp = emp.replace(" ", "%20")
-    datos = ""
-    temp = '/drug/label.json?search=manufacturer_name:"' + emp + '"&limit=100'
-    headers = {'User-Agent': 'http-client'}
-    conn = http.client.HTTPSConnection("api.fda.gov")
-    conn.request('GET', temp, None, headers)
-    r1 = conn.getresponse()
-    r2 = r1.read().decode("utf-8")
-    conn.close()
-    data = json.loads(r2)
 
-    if "results" in data:
-        for elem in data["results"]:
-            if "brand_name" in elem["openfda"]:
-                datos += (str(elem["openfda"]["brand_name"]).lower()[2:-2])
-                datos += "<br>"
-            else:
-                datos += "No hay resultados"
-                datos += "<br>"
+    direccion = OpenFDAClient()
+    respuesta = direccion.fda_connection('/drug/label.json?search=manufacturer_name:"' + emp + '"&limit=10')
 
-    else:
-        datos = "nada"
+    data1 = OpenFDAParser(respuesta)
+    data = data1.get_data()
 
-    return "<ul>{}</ul>".format(datos)
+    datos_html = OpenFDAHTML(data)
+    datos = datos_html.convert_into_html("openfda", "brand_name")
+
+    return datos
 
 @app.route("/listDrugs")
 def get_drugs():
-    num = request.args.get('limite')
-    print(num)
-    datos = ""
-    temp = '/drug/label.json?&limit=' + num
-    print(temp)
-    headers = {'User-Agent': 'http-client'}
-    conn = http.client.HTTPSConnection("api.fda.gov")
-    conn.request('GET', temp, None, headers)
-    r1 = conn.getresponse()
-    r2 = r1.read().decode("utf-8")
-    conn.close()
-    data = json.loads(r2)
+    num = request.args.get('limit')
 
-    if "results" in data:
-        for elem in data["results"]:
-            if "brand_name" in elem["openfda"]:
-                datos += (str(elem["openfda"]["brand_name"]).lower()[2:-2])
-                datos += "<br>"
-            else:
-                datos += "No hay resultados"
-                datos += "<br>"
+    direccion = OpenFDAClient()
+    respuesta = direccion.fda_connection('/drug/label.json?&limit=' + num)
 
-    else:
-        datos = "nada"
+    data1 = OpenFDAParser(respuesta)
+    data = data1.get_data()
 
-    return "<ul>{}</ul>".format(datos)
+    datos_html = OpenFDAHTML(data)
+    datos = datos_html.convert_into_html("openfda", "brand_name")
+
+    return datos
 
 @app.route("/listCompanies")
 def get_listcomp():
-    num = request.args.get('limite')
-    print(num)
-    datos = ""
-    temp = '/drug/label.json?&limit=' + num
-    print(temp)
-    headers = {'User-Agent': 'http-client'}
-    conn = http.client.HTTPSConnection("api.fda.gov")
-    conn.request('GET', temp, None, headers)
-    r1 = conn.getresponse()
-    r2 = r1.read().decode("utf-8")
-    conn.close()
-    data = json.loads(r2)
+    num = request.args.get('limit')
 
-    if "results" in data:
-        for elem in data["results"]:
-            if "manufacturer_name" in elem["openfda"]:
-                datos += (str(elem["openfda"]["manufacturer_name"]).lower()[2:-2])
-                datos += "<br>"
-            else:
-                datos += "No hay resultados"
-                datos += "<br>"
+    direccion = OpenFDAClient()
+    respuesta = direccion.fda_connection('/drug/label.json?&limit=' + num)
 
-    else:
-        datos = "nada"
+    data1 = OpenFDAParser(respuesta)
+    data = data1.get_data()
 
-    return "<ul>{}</ul>".format(datos)
+    datos_html = OpenFDAHTML(data)
+    datos = datos_html.convert_into_html("openfda", "manufacturer_name")
+
+    return datos
+
+@app.route("/listWarnings")
+def get_warnings():
+    num = request.args.get('limit')
+
+    direccion = OpenFDAClient()
+    respuesta = direccion.fda_connection('/drug/label.json?&limit=' + num)
+
+    data1 = OpenFDAParser(respuesta)
+    data = data1.get_data()
+
+    datos_html = OpenFDAHTML(data)
+    datos = datos_html.convert_into_html("warnings", "")
+
+    return datos
 
 @app.route("/")
 def do_get():
@@ -128,7 +167,7 @@ def do_get():
 
                 <h2>DRUG</h2>
 
-                <form action="/searchDrug">
+                <form action="searchDrug">
                   Medicamento:<br>
                   <input type="text" name="active_ingredient" value="">
                   <br>
@@ -145,7 +184,7 @@ def do_get():
 
                 <h2>COMPANY</h2>
 
-                <form action="/searchCompany">
+                <form action="searchCompany">
                   Empresa:<br>
                   <input type="text" name="company" value="">
                   <br>
@@ -162,9 +201,9 @@ def do_get():
 
                 <h2>LIST DRUGS</h2>
 
-                <form action="/listDrugs">
+                <form action="listDrugs">
                   Límite:<br>
-                  <input type="text" name="limite" value="">
+                  <input type="text" name="limit" value="">
                   <br>
                   <input type="submit" value="Submit">
                 </form> 
@@ -179,9 +218,9 @@ def do_get():
 
                 <h2>LIST COMPANIES</h2>
 
-                <form action="/listCompanies">
+                <form action="listCompanies">
                   Límite:<br>
-                  <input type="text" name="limite" value="">
+                  <input type="text" name="limit" value="">
                   <br>
                   <input type="submit" value="Submit">
                 </form> 
@@ -190,7 +229,36 @@ def do_get():
 
                 </body>
                 </html>"""
+
+    message += """<!DOCTYPE html>
+                      <html>
+                      <body>
+
+                      <h2>LIST WARNINGS</h2>
+
+                      <form action="listWarnings">
+                        Límite:<br>
+                        <input type="text" name="limit" value="">
+                        <br>
+                        <input type="submit" value="Submit">
+                      </form> 
+
+                      <p>If you click the "Submit" button, the form-data will be sent to a page called "/listWarnings".</p>
+
+                      </body>
+                      </html>"""
     return message
 
+
+@app.route('/secret')
+def no_autorizado():
+    content = 'WWW-Authenticate'
+    return content, status.HTTP_401_UNAUTHORIZED  #, {'WWWAuthenticate':'Basic realm="Login Required"'}
+
+@app.route('/redirect')
+def root():
+    return redirect('http://localhost:8000/', code=302)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8000)
